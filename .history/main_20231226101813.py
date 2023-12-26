0eb6a0b41e8d48a83fdd55fda476f5be24cfe473
Nandkumar Pawar <75560635+npawar8050@users.noreply.github.com>
2023-12-26 10:59:24 +0530

    cart_and_login
    same items added to cart are getting appended in same row.
    login dropdown menu added.

--- .history/main_20231226101813.py
+++ .history/main_20231226101813.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
